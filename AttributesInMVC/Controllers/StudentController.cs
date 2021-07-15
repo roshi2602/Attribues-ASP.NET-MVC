820e2a45a30a6cf7dabb6c4b95936324a597caf2
roshi2602 <roshidubey2602@gmail.com>
2021-07-15 13:29:19 +0530

    first commit

---
+++ AttributesInMVC/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributesInMVC.Models;
+
+namespace AttributesInMVC.Controllers
+{
+    //before starting refer STUDENT.cs because it has attributes
+    public class StudentController : Controller
+    {
+        STUDENTDBEntities db = new STUDENTDBEntities();
+        public ActionResult Index()
+        {
+            var x = db.STUDENTs.ToList();       //list of students fetched from database ->STUDENT model
+            return View(x);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int studentid)
+        {
+            var y = db.STUDENTs.FirstOrDefault(x => x.id == studentid);
+            return View(y);
+        }
+    }
+}
---
+++ AttributesInMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AttributesInMVC.MvcApplication" Language="C#" %>
